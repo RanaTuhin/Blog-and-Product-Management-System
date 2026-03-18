0aebbaaaad6e1561a4f4cba7933258713986043f
Tuhin Rana Monda <aimlesstuhin@gmail.com>
2026-03-18 23:01:46 +0530

    Updated Profile

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace App\Providers\Filament;
 
-use App\Filament\Livewire\SmtpSettingsForm;
 use Filament\Http\Middleware\Authenticate;
 use Filament\Http\Middleware\AuthenticateSession;
 use Filament\Http\Middleware\DisableBladeIconComponents;
@@ -85,7 +84,7 @@ class AdminPanelProvider extends PanelProvider
                         directory: 'avatars',
                     )
                     ->customProfileComponents([
-                        'smtp_settings_form' => SmtpSettingsForm::class,
+
                     ]),
             ]);
     }
